f040ee447376f236a94cabb304e65bf0c3a328e1
SergioMorero <smorero@mail.austral.edu.ar>
2025-10-01 00:00:16 -0300

    Initialized SpringBoot project

---
+++ src/main/kotlin/com/grupo14IngSis/snippetSearcherAccessManager/SnippetSearcherAccessManagerApplication.kt
@@ -0,0 +1,11 @@
+package com.grupo14IngSis.snippetSearcherAccessManager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SnippetSearcherAccessManagerApplication
+
+fun main(args: Array<String>) {
+    runApplication<SnippetSearcherAccessManagerApplication>(*args)
+}
